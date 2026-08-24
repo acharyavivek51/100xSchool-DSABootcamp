82691877d66cab0ad2f34732dac1bcc5bcba9428
Vivek Acharya <acharyavivek51@gmail.com>
2026-08-24 15:42:15 +0530

    output will print a floating - point output when either of the dividend or divisor are in decimal form.

---
+++ division.cpp
@@ -0,0 +1,15 @@
+// File        : division.cpp
+// Description : output will print a floating - point output when either of the dividend or divisor are in decimal form.
+// Language    : C++
+// Topic       : Arithmetic Operators
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << 10   + 3   << endl; // 13
+    cout << 10   - 3   << endl; // 7
+    cout << 10   * 3   << endl; // 30
+    cout << 10.0 / 3.0 << endl; // 3
+    cout << 10   % 3   << endl; // 1
+}
