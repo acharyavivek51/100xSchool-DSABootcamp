5fa9dedde6d80e865e617a5d78f5c0d89d4f7c73
Vivek Acharya <acharyavivek51@gmail.com>
2026-08-25 14:10:11 +0530

    Add input and addition program

---
+++ taking-input-from-user.cpp
@@ -0,0 +1,18 @@
+// File       : taking-input-from-user.cpp
+// Description: accepts input from user performs addition on it and prints on screen
+//Lecture     : Week 1 - DSA | Introduction to C++ | 15/01/2026
+// Topic      : taking input from user
+// Concepts   : Variables, cin, cout, arithmetic operators
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+
+    cin >> a;
+    cin >> b;
+
+    cout << a + b << endl;
+}
