0b2664de99b9e8287fa84bee67cc647d7e60c811
Vivek Acharya <acharyavivek51@gmail.com>
2026-09-02 14:37:51 +0530

    Add reverse number solutions

---
+++ Codeforces-GroupContests/B-Loops/K-Print-Number-in-Reverse.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+
+    if(n ==0)
+    {
+        cout << 0;
+    }
+
+    while(n != 0)
+    {
+        cout << n % 10;
+        n = n / 10;
+    }
+}
